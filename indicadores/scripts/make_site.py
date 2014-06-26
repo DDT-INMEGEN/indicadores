d9183cd6d4c128f811c54a96896378da7faf2da7
Mariana Velasco <mariana@crunchbang>
2014-06-26 13:52:37 -0500

    Agrego Avace Tesis

--- indicadores/scripts/make_site.py
+++ indicadores/scripts/make_site.py
@@ -13,7 +13,9 @@ reportes = [
     { 'reporte': 'rspi_status.html',  'template': 'dashboard_investigacion.html' },
     { 'reporte': 'rspi_investigadores.html',  'template': 'dashboard_investigacion.html' },
     { 'reporte': 'rspi_proyectos_costos_sunburst.html',  'template': 'dashboard_investigacion_d3.html' },
+    { 'reporte': 'rspi_proyectos_convenio.html',  'template': 'dashboard_investigacion.html' },
     { 'reporte': 'rspi_usuarios_puesto.html',  'template': 'dashboard_investigacion.html' },
+    { 'reporte': 'rspi_tesis_avance.html',  'template': 'dashboard_investigacion.html' },
 
     { 'reporte': "tg_categorias.html", 'template': 'dashboard_tg.html' },
     { 'reporte': "tg_estados.html", 'template': 'dashboard_tg.html' },
