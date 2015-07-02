c0f176863a71290861d4b8729ad4d5bd4284cf37
absorto <rgarcia@inmegen.gob.mx>
2015-07-02 13:14:12 -0500

    indicadores nuevos
    Personal por area
    personal por puesto
    
    proyectos por linea
    publicaciones por autor

--- indicadores/scripts/make_site.py
+++ indicadores/scripts/make_site.py
@@ -17,7 +17,10 @@ reportes = [
     { 'reporte': 'rspi_usuarios_puesto.html',  'template': 'dashboard_investigacion.html' },
     { 'reporte': 'rspi_usuarios_area.html',  'template': 'dashboard_investigacion.html' },
     { 'reporte': 'rspi_tesis_avance.html',  'template': 'dashboard_investigacion.html' },
-    { 'reporte': 'rspi_lineas_proyectos.html',  'template': 'dashboard_investigacion_d3.html' },
+#    { 'reporte': 'rspi_lineas_proyectos.html',  'template':
+#    'dashboard_investigacion_d3.html' },
+    { 'reporte': 'rspi_proyectos_por_linea.html',  'template': 'dashboard_investigacion.html' },
+    { 'reporte': 'rspi_publicaciones_por_autor.html',  'template': 'dashboard_investigacion.html' },    
 
     { 'reporte': "tg_categorias.html", 'template': 'dashboard_tg.html' },
     { 'reporte': "tg_estados.html", 'template': 'dashboard_tg.html' },
@@ -30,6 +33,8 @@ reportes = [
     { 'reporte': 'ti_personas.html',  'template': 'dashboard_ti.html' },
     { 'reporte': 'ti_dinamica.html',  'template': 'dashboard_ti.html' },
 
+    { 'reporte': 'admin_personal_puestos.html',  'template': 'dashboard_admin_intranet.html' },
+    { 'reporte': 'admin_personal_areas.html',  'template': 'dashboard_admin_intranet.html' },     
     { 'reporte': 'admin_cfe.html',  'template': 'dashboard_admin.html' },
     { 'reporte': 'admin_impresoras_dinamica.html',  'template': 'dashboard_admin.html' },
     { 'reporte': 'admin_impresoras_areas.html',  'template': 'dashboard_admin.html' },
