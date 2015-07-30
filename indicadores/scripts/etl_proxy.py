926174d2ee6d4a2f0ac9d555566a3abd281855ad
absorto <rgarcia@inmegen.gob.mx>
2015-07-29 20:59:32 -0500

    load log to RDB

--- indicadores/scripts/etl_proxy.py
+++ indicadores/scripts/etl_proxy.py
@@ -3,7 +3,7 @@ import argparse
 import time, datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, backref
-from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey, create_engine
+from sqlalchemy import Column, Integer, Unicode, Date, Time, ForeignKey, create_engine
 from urlparse import urlparse
 
 from pprint import pprint
@@ -27,29 +27,31 @@ class Request(Base):
 
     date          = Column(Date)
     time          = Column(Time)
-    client_id     = Column(Integer, ForeignKey('clients.id'))
-    
-    url_scheme    = Column(String)
-    server_id     = Column(Integer, ForeignKey('servers.id'))
-    url_file_type = Column(String)
+
+    url_scheme    = Column(Unicode)
+    url_file_type = Column(Unicode)
     url_port      = Column(Integer)
     
-    method        = Column(String)
+    method        = Column(Unicode)
     result        = Column(Integer)
 
-    comment       = Column(String)
+    comment       = Column(Unicode)
 
+    client_id     = Column(Integer, ForeignKey('clients.id'))
+    server_id     = Column(Integer, ForeignKey('servers.id'))    
+
+    
 
 class Client(Base):
     __tablename__ = 'clients'
     id            = Column(Integer, primary_key=True)
-    addr          = Column(String)
+    addr          = Column(Unicode)
     requests      = relationship("Request", backref="client")
     
 class Server(Base):
     __tablename__ = 'servers'
     id            = Column(Integer, primary_key=True)
-    addr          = Column(String)
+    addr          = Column(Unicode)
     requests      = relationship("Request", backref="server")
 
 
@@ -67,18 +69,17 @@ if args.init == 'true':
     Base.metadata.create_all(engine)
 
 
+session = Session()
 
-
-
-#################
-# Read log file #
-#################
+##################
+# Parse log file #
+##################
 for l in args.log.readlines():
-    fields = l.split(' ')
+    fields = l.decode('utf-8').split(' ')
     
     day  = datetime.date(*time.strptime( fields[0], "%Y.%m.%d" )[:3])
     hour = datetime.time(*time.strptime( fields[1], "%H:%M:%S" )[3:6])
-    client = fields[3]
+    client_addr = fields[3]
 
     # parse url
     url    = urlparse(fields[4])
@@ -92,34 +93,61 @@ for l in args.log.readlines():
     
         
     if fields[5] == "*DENIED*":
-        if fields[6] == "Extensión" and fields[7] == "bloqueada:":
+        if fields[6] == u"Extensión" and fields[7] == "bloqueada:":
             method = fields[9]
             result = fields[15]
-            comment = " ".join(fields[6:9])
+            comment = u" ".join(fields[6:9])
         elif fields[6] == 'URL' and fields[7] == "bloqueada":
             method = fields[12]
             result = fields[20]
-            comment = " ".join(fields[6:11]) + url.path
+            comment = u" ".join(fields[6:11]) + url.path
         elif fields[9] == 'Bloqueo' and fields[10] == 'general':
             method = fields[27]
             result = fields[32]
-            comment = " ".join(fields[6:26])
+            comment = u" ".join(fields[6:26])
         else:
             method = fields[10]
             result = fields[16]
-            comment = " ".join(fields[6:10])
+            comment = u" ".join(fields[6:10])
     elif fields[5] == "*EXCEPTION*":
-        if fields[6] == "Dirección" and fields[7] == "IP":
+        if fields[6] == u"Dirección" and fields[7] == "IP":
             method =  fields[16]
             result = fields[21]
-            comment = " ".join(fields[6:16])
+            comment = u" ".join(fields[6:16])
         elif fields[6] == "Sitio" and fields[7] == "presente":
             method = fields[13]
             result = fields[18]
-            comment = " ".join(fields[6:13]) 
+            comment = u" ".join(fields[6:13]) 
     else:
         result = fields[11]
         method = fields[6]
-        comment = ""
+        comment = u""
 
-    #print day,hour,method,result
+
+    ######################
+    # Upsert to database #
+    ######################        
+    request = Request( date          = day,
+                       time          = hour,
+                       url_scheme    = url_scheme,
+                       url_file_type = url_file_type,
+                       url_port      = url_port,
+                       method        = method,
+                       result        = result,
+                       comment       = comment )
+
+        
+    server = session.query(Server).filter(Server.addr == url_netloc).first()
+    if not(server):
+        server = Server(addr = url_netloc)
+        session.add(server)
+    server.requests.append( request )
+
+    client = session.query(Client).filter(Client.addr == client_addr).first()
+    if not(client):
+        client = Client(addr = client_addr)
+        session.add(client)
+    client.requests.append( request )
+
+    
+    session.commit()
