42c100c6c0e2712b3e15097d3b4bf5f3ecdd3579
absorto <rgarcia@inmegen.gob.mx>
2016-04-12 13:09:49 -0500

    regenerar red de mesh en el make

--- indicadores/make.sh
+++ indicadores/make.sh
@@ -11,3 +11,6 @@ python scripts/rspi_costos.py
 
 echo "Generating static html"
 python scripts/make_site.py
+
+echo "Generating mesh-term network"
+python scripts/etl_pubmed.py | python scripts/mesh_json.py > static/rspi_mesh.json
